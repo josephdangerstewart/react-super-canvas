581326f47c089cb554bfc6ad8baf96934f096d1e
Joseph Stewart <joseph.s@nationalschoolproject.com>
2019-10-20 21:47:03 -0700

    Add public ISuperCanvasManager interface

--- src/types/ISuperCanvasManager.ts
+++ src/types/ISuperCanvasManager.ts
@@ -1,1 +1,6 @@
-
+export default interface ISuperCanvasManager {
+	/**
+	 * @description Initializes the super canvas manager with the canvas element
+	 */
+	init: (canvas: HTMLCanvasElement) => void;
+}
