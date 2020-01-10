7a59277c5895909fa9a93c57234779da53f67974
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-01-10 12:25:47 -0800

    Add tests for rectToPoints and rectToLines

--- tests/unit-tests/shapes-utility-unit-tests.ts
+++ tests/unit-tests/shapes-utility-unit-tests.ts
@@ -3,8 +3,19 @@ import Line from '../../src/types/shapes/Line';
 import {
 	polygonToLines,
 	vector,
+	rectToPoints,
+	rectToLines,
 } from '../../src/utility/shapes-util';
 import { line } from '../test-utils/shapes';
+import Rectangle from '../../src/types/shapes/Rectangle';
+import Vector2D from '../../src/types/utility/Vector2D';
+
+// Prepare a 5x5 rectangle with top left corner of (0,0)
+const rect: Rectangle = Object.freeze({
+	topLeftCorner: vector(0, 0),
+	width: 5,
+	height: 5,
+});
 
 describe('shapes-util', () => {
 	describe('polygonToLines', () => {
@@ -40,6 +51,36 @@ describe('shapes-util', () => {
 			const lines = polygonToLines(polygon, true);
 
 			expect(lines).toEqual(expectedLines);
+		});
+	});
+
+	describe('rectToPoints', () => {
+		const expectedPoints: Vector2D[] = [
+			vector(0, 0),
+			vector(5, 0),
+			vector(0, 5),
+			vector(5, 5),
+		];
+
+		it('can convert a rectangle to an array of points', () => {
+			const result = rectToPoints(rect);
+
+			expect(result).toEqual(expectedPoints);
+		});
+	});
+
+	describe('rectToLines', () => {
+		const expectedLines: Line[] = [
+			line(0, 0, 5, 0),
+			line(5, 0, 5, 5),
+			line(5, 5, 0, 5),
+			line(0, 5, 0, 0),
+		];
+
+		it('can convert a rectangle to an array of lines', () => {
+			const result = rectToLines(rect);
+
+			expect(result).toEqual(expectedLines);
 		});
 	});
 });
