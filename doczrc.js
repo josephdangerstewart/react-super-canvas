4d4d428108b7a30c2922b684828687cac26c2509
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-02-05 18:00:12 -0800

    Manually organize the sidebar menus

--- doczrc.js
+++ doczrc.js
@@ -3,4 +3,29 @@
 export default {
 	typescript: true,
 	ignore: [ 'README.md' ],
+	menu: [
+		'Welcome',
+		{
+			name: 'Components',
+			menu: [
+				'The Super Canvas',
+				'Extending the Toolbar',
+			]
+		},
+		{
+			name: 'API Reference',
+			menu: [
+				'IPainterAPI',
+				'IBrush',
+				'Shape Types',
+				'Utility Types',
+			]
+		},
+		{
+			name: 'Utility Library',
+			menu: [
+				'Shape Utility',
+			],
+		},
+	],
 };
