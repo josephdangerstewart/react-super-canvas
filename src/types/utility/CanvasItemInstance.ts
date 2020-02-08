b8b55fbda10442cedf975893000cb47f54d33278
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-02-07 20:49:07 -0800

    Remove canvasiteminstance

--- src/types/utility/CanvasItemInstance.ts
+++
@@ -1,9 +0,0 @@
-import ICanvasItem from '../ICanvasItem';
-
-/**
- * @description This interface adds a wrapper around ICanvas item so
- * the super canvas manager can add it's own metadata to it
- */
-export default interface CanvasItemInstance extends ICanvasItem {
-	$rotation: number;
-}
