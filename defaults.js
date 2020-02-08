001fb352318d89bb2d5ce426aae0c13e9dc43eef
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-02-07 20:53:44 -0800

    Add entry points in package root

---
+++ defaults.js
@@ -0,0 +1,14 @@
+// Expose canvas items
+export { CircleCanvasItem } from './dist/defaults';
+export { ImageCanvasItem } from './dist/defaults';
+export { PolygonCanvasItem } from './dist/defaults';
+
+// Expose brushes
+export { CircleBrush } from './dist/defaults';
+export { ImageBrush } from './dist/defaults';
+export { PolygonBrush } from './dist/defaults';
+export { SelectionBrush } from './dist/defaults';
+
+// Exposes background elements
+export { DefaultBackgroundElement } from './dist/defaults';
+export { GridBackground } from './dist/defaults';
---
+++ index.js
@@ -0,0 +1,1 @@
+export { SuperCanvas } from './dist/index';
