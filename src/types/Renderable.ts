04056884df86ab22d483f870635314c658edb5cf
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-07-17 18:01:54 -0700

    Add documentation for renderable type

--- src/types/Renderable.ts
+++ src/types/Renderable.ts
@@ -2,18 +2,52 @@ import Circle from './shapes/Circle';
 import Rectangle from './shapes/Rectangle';
 import Line from './shapes/Line';
 import Polygon from './shapes/Polygon';
+import JsonData from './utility/JsonData';
 
 export interface Renderable {
+	/**
+	 * @description The stack of render operations that make up this renderable item.
+	 * A renderable item is a singular and serializable format to describe any given
+	 * rendered object on the canvas.
+	 */
 	ops: RenderOp[];
+
+	/**
+	 * @description The output from `toJson` used to try and map this renderable back
+	 * to a canvas item
+	 */
+	canvasItemJson: JsonData;
 }
 
 export interface RenderOp {
+	/**
+	 * @description The type of object that will be rendered to the canvas
+	 */
 	type: RenderOpType;
 
+	/**
+	 * @description Circle data if type is `RenderOpType.Circle`
+	 */
 	circle?: Circle;
+
+	/**
+	 * @description Rectangle data if type is `RenderOpType.Rectangle`
+	 */
 	rect?: Rectangle;
+
+	/**
+	 * @description Line data if type is `RenderOpType.Line`
+	 */
 	line?: Line;
+
+	/**
+	 * @description Polygon data if type is `RenderOpType.Polygon`
+	 */
 	polygon?: Polygon;
+
+	/**
+	 * @description Image data if type is `RenderOpType.Image`
+	 */
 	image?: {
 		boundingRect: Rectangle;
 		imageUrl: string;
